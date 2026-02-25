86524c3758bfc64fa33a75d14e6f877bd7a9c77f
m.uppenkamp <35469083+MUppenkamp@users.noreply.github.com>
2026-02-25 23:57:46 +0100

    remove unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,6 @@ module.exports = {
     },
     extend: {
       colors: {
-        'wedding-rose': '#F0B7C8',
-        'wedding-peach': '#F4C7B4',
-        'wedding-mint': '#BFE9D8',
-        'wedding-lilac': '#D4B8F0',
-        'wedding-ivory': '#FAF3EE',
-        'bouquet-red': '#e73520',
-        'bouquet-lilac': '#A72F81',
-        'bouquet-yellow': '#fcd764',
-        'bouquet-orange': '#ea4622',
-        'bouquet-pink': '#dc263e',
-        'bouquet-red-pastel': '#F4C3CE',
-        'bouquet-lilac-pastel': '#DECDF6',
-        'bouquet-yellow-pastel': '#F7E2A8',
-        'bouquet-orange-pastel': '#F7D0B5',
-        'bouquet-pink-pastel': '#F1C6DE',
-        'bouquet-ivory': '#FAF3EE',
-
         'jga-background': '#fdf6f5',
         'jga-text': '#7d6f62',
         'jga-blue': '#99beee',
